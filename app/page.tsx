72659e1927020a3ccb62838e843d98dacf26d2eb
aksisonline <teamscientify2016@gmail.com>
2025-02-06 12:19:43 +0530

    Revert "Implement project prefetching and add project data structure"
    This reverts commit 81313f1e5053e924989496ccd0eeeb1333d6dc36.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-"use client"
-import { usePrefetchProjects } from '@/hooks/usePrefetchProjects';
-
 export default function Home() {
-  usePrefetchProjects();
-  
   return (
     <div className="flex flex-col justify-center h-full max-w-2xl mx-auto p-4 md:p-0">
       <h1 className="text-[4rem] md:text-[8rem] leading-none font-bold tracking-tight">
